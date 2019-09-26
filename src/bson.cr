553f3112a0acc52c8f2eeab86dfad078653f8e7c
Sal Scotto <sal.scotto@gmail.com>
2019-09-26 14:34:00 -0600

    Memory Leak Fixes and Associted Options for the driver

--- src/bson.cr
+++ src/bson.cr
@@ -173,7 +173,7 @@ class BSON
   end
 
   def []=(key, value : Time)
-    LibBSON.bson_append_date_time(handle, key, key.bytesize, value.to_utc.epoch * 1000)
+    LibBSON.bson_append_date_time(handle, key, key.bytesize, value.to_utc.to_unix * 1000)
   end
 
   def []=(key, value : Timestamp)
