8a36631f0d7e0d87b80b47773cd6b7366fc0a75b
Sal Scotto <sal.scotto@gmail.com>
2019-10-07 14:51:30 -0600

    added to_json extended format for binary block

--- src/bson/binary.cr
+++ src/bson/binary.cr
@@ -1,3 +1,4 @@
+require "base64"
 class BSON
   struct Binary
     enum SubType
@@ -15,7 +16,23 @@ class BSON
     end
 
     def to_json(json : JSON::Builder)
-      json.string "<BINARY>"
+        json.object do
+            json.field("$binary") do
+                json.object do
+                    json.field "base64" do
+                        json.string Base64.encode(@data)
+                    end
+                    json.field "subtype",@subtype
+                end
+            end
+        end
+          # { "$binary":
+                  #   {
+                  #      "base64": "<payload>",
+                  #      "subtype": "<t>"
+                  #   }
+                  #}
+          #
     end
 
     def to_raw_type
