76f94bf84f432a77056e4ca67cc521221ed19fbe
Sal Scotto <sal.scotto@gmail.com>
2020-03-12 10:12:00 -0600

    change hook back

--- src/bson/core_ext/array.cr
+++ src/bson/core_ext/array.cr
@@ -1,20 +1,20 @@
 class Array(T)
-  def to_bson(bson = BSON.new)
-    #return BSON.from_json(self.to_json)
-    each_with_index do |item, i|
-          case item
-          when Array
-            bson.append_array(i.to_s) do |appender, child|
-              item.to_bson(child)
-            end
-          when Hash
-            bson.append_document(i.to_s) do |child|
-              item.to_bson(child)
-            end
-          else
-            bson[i.to_s] = item
-          end
-        end
-        bson
+  def to_bson#(bson = BSON.new)
+    return BSON.from_json(self.to_json)
+    #each_with_index do |item, i|
+    #      case item
+    #      when Array
+    #        bson.append_array(i.to_s) do |appender, child|
+    #          item.to_bson(child)
+    #        end
+    #      when Hash
+    #        bson.append_document(i.to_s) do |child|
+    #          item.to_bson(child)
+    #        end
+    #      else
+    #        bson[i.to_s] = item
+    #      end
+    #    end
+    #    bson
   end
 end
--- src/bson/core_ext/hash.cr
+++ src/bson/core_ext/hash.cr
@@ -1,21 +1,21 @@
 require "json"
 class Hash(K, V)
-  def to_bson(bson = BSON.new)
-    #return BSON.from_json(self.to_json)
-     each do |k, v|
-          case v
-          when Array
-            bson.append_array(k) do |appender, child|
-              v.to_bson(child)
-            end
-          when Hash
-            bson.append_document(k) do |child|
-              v.to_bson(child)
-            end
-          else
-            bson[k] = v
-          end
-        end
-        bson
+  def to_bson#(bson = BSON.new)
+    return BSON.from_json(self.to_json)
+    #each do |k, v|
+    #      case v
+    #      when Array
+    #        bson.append_array(k) do |appender, child|
+    #          v.to_bson(child)
+    #        end
+    #      when Hash
+    #        bson.append_document(k) do |child|
+    #          v.to_bson(child)
+    #        end
+    #      else
+    #        bson[k] = v
+    #      end
+    #    end
+    #    bson
   end
 end
