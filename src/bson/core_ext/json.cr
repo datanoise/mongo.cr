bd1b509016e5b8287e28fbccf6ad8b309b411552
elbywan <elbywan@hotmail.com>
2020-03-20 12:14:47 +0100

    fix JSON::Any serialization issues (again)

--- src/bson/core_ext/json.cr
+++ src/bson/core_ext/json.cr
@@ -1,10 +1,25 @@
 struct JSON::Any
   def to_bson
-    BSON.from_json self.to_json
+    case raw = self.raw
+    when Nil
+      raw
+    when Bool
+      raw
+    when Int64
+      raw
+    when Float64
+      raw
+    when String
+      raw
+    else
+      BSON.from_json self.to_json
+    end
   end
 
   def self.from_bson(bson : BSON::Field) : self
     case bson
+    when Nil
+      self.new bson
     when Bool
       self.new bson
     when Int64
